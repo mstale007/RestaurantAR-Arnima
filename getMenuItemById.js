35a578e49330b02f01b45ed61ba92178fe9f0706
mstale <mstale20@gmail.com>
2022-12-05 21:47:16 +0530

    Added ui popup

---
+++ getMenuItemById.js
@@ -0,0 +1,33 @@
+get_menuitems_by_id={
+    "id1":{qty:2,price:100},
+    "id2":{qty:2,price:200},
+}
+get_menuitems_by_id_reponse={
+    'data':[
+        {
+            "id":"id1",
+            "type":"model",
+            "name":"Veg. Pizza",
+            "price":150,
+            "category":"Pizza Mania",
+            "description":"Enjoy fresh pan veg pizza with paneer, onion and capsicum toppings",
+            "model_link":"resources/models/hotdog2.glb",
+            "preview_image":"https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+            "food_category":"veg",
+            "ar_enabled":true
+        },
+        {
+            "id":"id2",
+            "type":"model",
+            "name":"Non-Veg. Pizza",
+            "price":200,
+            "category":"Pizza Mania",
+            "description":"Enjoy fresh pan veg pizza with chicken, onion and capsicum toppings",
+            "model_link":"resources/models/fried_egg_sandwich2.glb",
+            "preview_image":"https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+            "food_category":"non_veg",
+            "ar_enabled":true
+        },
+    ],
+    'price_changed':true,
+}
